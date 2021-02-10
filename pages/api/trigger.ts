6161ba9ea97a3292b6764af12b78b93fc487e3bb
zzwt <loophone@gmail.com>
2021-02-10 15:32:54 +1100

    Change endpoint to post method

--- pages/api/trigger.ts
+++ pages/api/trigger.ts
@@ -2,11 +2,15 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import { BarchartCrawler } from './../../utils/crawler';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const { AppSecret } = req.body;
-  if (AppSecret === process.env.APP_SECRET) {
-    const crawler = new BarchartCrawler();
-    await crawler.start();
-    return res.send({ msg: 'success' });
+  if (req.method === 'POST') {
+    const { AppSecret } = req.body;
+    if (AppSecret === process.env.APP_SECRET) {
+      const crawler = new BarchartCrawler();
+      await crawler.start();
+      return res.send({ msg: 'success' });
+    }
+    return res.send({ msg: 'Secret is not corrent', error: true });
+  } else {
+    return res.send({ msg: 'Method not supported', error: true });
   }
-  return res.send({ msg: 'Secret is not corrent', error: true });
 };
